29139ba74ac24618a85621f82900fdbe7df377fa
EndeavourHolmes <zisbrandt@gmail.com>
2022-02-16 14:29:37 +0100

    Update: Ranking max 10

--- app/src/main/java/com/example/koffer/SQLdb.java
+++ app/src/main/java/com/example/koffer/SQLdb.java
@@ -36,21 +36,56 @@ public class SQLdb extends SQLiteOpenHelper {
     }
 
     public boolean addOne(String name, int score, int neededTime){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues cv = new ContentValues();
 
-        cv.put(COLUMN_NAME, name);
-        cv.put(COLUMN_SCORE, score);
-        cv.put(COLUMN_NEEDEDTIME, neededTime);
+        // Check more than 10 entries in ranking
+        if (countEntries()>9){
 
-        long insert = db.insert(RANKING_TABLE,null , cv);
-        if (insert == -1){
-            db.close();
-            return false;
+            // Check if old highscores lower than actual one
+            if (compareNewHighscore(score,neededTime) == 404){
+                return true;
+            }
+
+            // Update the entry (which is last one when sorted), when new highscore is better than one of the ten
+            else {
+                int updateID = compareNewHighscore(score,neededTime);
+                String id = String.valueOf(updateID);
+
+                SQLiteDatabase db = this.getWritableDatabase();
+                ContentValues cv = new ContentValues();
+
+                cv.put(COLUMN_NAME, name);
+                cv.put(COLUMN_SCORE, score);
+                cv.put(COLUMN_NEEDEDTIME, neededTime);
+
+                long insert = db.update(RANKING_TABLE, cv, COLUMN_ID + " = ?", new String[]{id});
+
+                if (insert == -1){
+                    db.close();
+                    return false;
+                }
+                else {
+                    db.close();
+                    return true;
+                }
+            }
         }
         else {
-            db.close();
-            return true;
+
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues cv = new ContentValues();
+
+            cv.put(COLUMN_NAME, name);
+            cv.put(COLUMN_SCORE, score);
+            cv.put(COLUMN_NEEDEDTIME, neededTime);
+
+            long insert = db.insert(RANKING_TABLE, null, cv);
+            if (insert == -1) {
+                db.close();
+                return false;
+            } else {
+                db.close();
+                return true;
+            }
         }
     }
 
@@ -64,16 +99,18 @@ public class SQLdb extends SQLiteOpenHelper {
         Cursor cursor = db.rawQuery(queryString, null);
 
         String ausgabe;
+        int position = 0;
 
         if (cursor.moveToFirst()){
             do {
                 ausgabe="";
+                position++;
                 int userId = cursor.getInt(0);
                 String name = cursor.getString(1);
                 int score = cursor.getInt(2);
                 int neededTime = cursor.getInt(3);
 
-                ausgabe += name + " Score: " + score + " Time: " + neededTime;
+                ausgabe += position + ": " + name + " Score: " + score + " Time: " + neededTime;
                 returnList.add(ausgabe);
 
             }while (cursor.moveToNext());
@@ -86,6 +123,60 @@ public class SQLdb extends SQLiteOpenHelper {
         db.close();
 
         return returnList;
+    }
+
+    public int countEntries(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String queryString = "SELECT count(*) FROM " + RANKING_TABLE;
+        Cursor mCount= db.rawQuery(queryString, null);
+        mCount.moveToFirst();
+        int count= mCount.getInt(0);
+        mCount.close();
+        db.close();
+        return count;
+    }
+
+    public int compareNewHighscore(int newScore, int newTime){
+
+        String queryString = "SELECT * FROM " + RANKING_TABLE + " ORDER BY " + COLUMN_SCORE + " DESC, " + COLUMN_NEEDEDTIME + " ASC";
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        int lowerId = 404;
+
+        if (cursor.moveToFirst()){
+            do {
+                int userId = cursor.getInt(0);
+                String name = cursor.getString(1);
+                int score = cursor.getInt(2);
+                int neededTime = cursor.getInt(3);
+
+                if (newScore > score){
+                    cursor.moveToLast();
+                    userId = cursor.getInt(0);
+                    lowerId = userId;
+                    break;
+                }
+                if (newScore == score){
+                    if (newTime < neededTime){
+                        cursor.moveToLast();
+                        userId = cursor.getInt(0);
+                        lowerId = userId;
+                        break;
+                    }
+                }
+
+            }while (cursor.moveToNext());
+
+        }
+        else {
+
+        }
+        cursor.close();
+        db.close();
+
+        return lowerId;
     }
 
     public void deleteAll(){
